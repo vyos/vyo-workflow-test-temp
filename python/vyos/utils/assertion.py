e5fa3e15e47651c581e19d9ed7ceeb32e97962c9
Vijayakumar A <36878324+kumvijaya@users.noreply.github.com>
2024-10-26 12:55:26 +0530

    T6786: TEST

--- python/vyos/utils/assertion.py
+++ python/vyos/utils/assertion.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
+
 def assert_boolean(b):
     if int(b) not in (0, 1):
         raise ValueError(f'Value {b} out of range')
